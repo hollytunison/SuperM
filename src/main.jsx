eee948c1c6699ce507ca1a61a32743ade304ef4e
Holly Tunison <hollytunison@gmail.com>
2023-02-05 12:37:06 -0500

    deleted starter files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import {createRoot} from "react-dom/client";
+import Button from "./Button.js"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+function App() {
+    function handleButtonClick() {
+        console.log("Button clicked");
+    }
+
+    // here we provide you with sample usages of the Button component
+    // feel free to add your own
+     return (<div style={{display: "flex", alignItems: "center", gap: "20px", flexWrap: "wrap"}}>
+        <Button>Normal</Button>
+        <Button outline>Outline</Button>
+        <Button className="extra-class" onClick={handleButtonClick}>Customizable</Button>
+    </div>);
+}
+
+createRoot(document.querySelector("#react-root")).render(<App />);
