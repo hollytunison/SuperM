1d0b23525c7399271063c905b33621ba7f9a7f33
Holly Tunison <hollytunison@gmail.com>
2023-02-06 10:14:13 -0500

    fixed styles folder

--- src/index.jsx
+++ src/index.jsx
@@ -7,26 +7,26 @@ import Navbar from "./Navbar.jsx";
 import About from "./About.jsx";
 import Products from "./Products.jsx";
 import Cart from "./Cart.jsx";
+import "/styles.css";
 
+  function App() {
+    return (
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />}>
+            </Route>
+            <Route path="/about" element={<About />}>
+            </Route>
+            <Route path="/products" element={<Products />}>
+            </Route>
+            <Route path="/cart" element={<Cart />}>
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />}>
-          </Route>
-          <Route path="/about" element={<About />}>
-          </Route>
-          <Route path="/products" element={<Products />}>
-          </Route>
-          <Route path="/cart" element={<Cart />}>
-          </Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-}
-
-createRoot(document.querySelector("#react-root")).render(<React.StrictMode><App /></React.StrictMode>);
+  createRoot(document.querySelector("#react-root")).render(<React.StrictMode><App /></React.StrictMode>);
